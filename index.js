6eb1f76d68470ada0ff8d00fdc5e15e63b478ff9
Alexander (Sasha) Borodin <sasha@borodin.info>
2026-05-17 00:30:05 -0600

    Update package.json for publishing.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Device = require('./Device');
+const { name: pluginName } = require('./package.json');
 
 let Service, Characteristic;
 
@@ -6,7 +7,7 @@ let Service, Characteristic;
 module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory('@bjclopes/homebridge-ledstrip-bledom', 'LedStrip', LedStrip);
+  homebridge.registerAccessory(pluginName, 'LedStrip', LedStrip);
 };
 
 function LedStrip(log, config, api) {
